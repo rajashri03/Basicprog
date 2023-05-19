6af112431ceb16f0747d20067203b6c14c8d1e9c
rajashri03 <telvekar.rajashri@gmail.com>
2023-05-19 09:10:27 +0530

    [Rajashri]-Added flip coin

---
+++ BasicCoreProgramming/HeadTails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCoreProgramming
+{
+    internal class HeadTails
+    {
+        public void CheckHeadTails()
+        {
+            int tailcount = 0, headcount = 0;
+            Console.WriteLine("********Flip coin and print percentage**********");
+            Console.WriteLine("Enter No. of times you want to flip the coin");
+            int NumofCount = Convert.ToInt32(Console.ReadLine());
+
+            Random randomobj = new Random();
+            for (int i=0;i<NumofCount;i++)
+            {
+                double randomheadtail=randomobj.NextDouble();
+                if(randomheadtail<0.5)
+                {
+                    Console.WriteLine("Tail");
+                    tailcount++;
+                }
+                else
+                {
+                    Console.WriteLine("HEad");
+                    headcount++;
+                }
+               
+            }
+            Console.WriteLine("------Percentage of Head vs Tail-------");
+            double perOfHead = (headcount / NumofCount) * 100;
+            Console.WriteLine("Percentage of head=" + perOfHead);
+            double perOfTail = (tailcount / NumofCount) * 100;
+            Console.WriteLine("Percentage of tail=" + perOfTail);
+        }
+    }
+}
---
+++ BasicCoreProgramming/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BasicCoreProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           HeadTails headTailsobj = new HeadTails();
+            headTailsobj.CheckHeadTails();
+
+        }
+    }
+}
